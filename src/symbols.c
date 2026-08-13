52200a22199bf4db6ded726afa0735d3eb192ab3
Paul J. Lucas <paul@lucasmail.org>
2026-08-13 14:29:08 -0700

    Reduced estimated number of symbols.

--- src/symbols.c
+++ src/symbols.c
@@ -1626,7 +1626,7 @@ static void visit_OverloadedDeclRef( CXCursor overloaded_csr, CXCursor parent,
 void symbols_init( void ) {
   ASSERT_RUN_ONCE();
   ht_init(
-    &symbol_set, HT_DINT, 2.0, 1024,
+    &symbol_set, HT_DINT, 2.0, 128,
     POINTER_CAST( ht_cmp_fn_t, &tidy_symbol_cmp ),
     POINTER_CAST( ht_hash_fn_t, &tidy_symbol_hash )
   );
