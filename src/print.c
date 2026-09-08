47a01618ae47de67aa6a1cc13123076c7888a378
Paul J. Lucas <paul@lucasmail.org>
2026-09-08 10:58:26 -0700

    Eliminated a use of fputs_quoted().

--- src/print.c
+++ src/print.c
@@ -295,11 +295,8 @@ void verbose_print_argv( char const *label, int argc,
                          char const *const argv[] ) {
   verbose_section_begin( /*printed_header=*/NULL );
   verbose_printf( "%s argv:\n", label );
-  for ( int i = 0; i < argc; ++i ) {
-    verbose_printf( "  %2d = ", i );
-    fputs_quoted( argv[i], '"', stdout );
-    putchar( '\n' );
-  } // for
+  for ( int i = 0; i < argc; ++i )
+    verbose_printf( "  %2d = \"%s\"\n", i, argv[i] );
 }
 // LCOV_EXCL_STOP
 
