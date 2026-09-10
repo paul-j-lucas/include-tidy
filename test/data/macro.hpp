42f1f3a2a1196db98a0210f8c44baba34284d746
Paul J. Lucas <paul@lucasmail.org>
2026-09-09 20:42:11 -0700

    Better macro test.

--- test/data/macro.hpp
+++ test/data/macro.hpp
@@ -1,6 +1,12 @@
 #ifndef macro_hpp
 #define macro_hpp
 
+#include <iostream>
+
+#define internal_error \
+  std::cerr << (me) << ", \"" \
+  << __FILE__ << "\", line " << __LINE__ << ": internal error: "
+
 #define ZERO(/**/TYPE,SIZE)       (TYPE){ .count = 0, .size = (SIZE) }
 
 #endif /* macro_hpp */
