8c5b79d535093c17687b152d5479dcda1f2a7add
Paul J. Lucas <paul@lucasmail.org>
2026-09-15 08:49:14 -0700

    Updated test files: more comprehensive.

--- test/data/file_info.hpp
+++ test/data/file_info.hpp
@@ -6,20 +6,84 @@
  * @sa https://github.com/paul-j-lucas/swishxx
  */
 
-#ifndef swishxx+file_info_hpp
-#define swishxx+file_info_hpp
+#ifndef file_info_hpp
+#define file_info_hpp
 
+#include "hash.hpp"
+
+#include <cstddef>
 #include <vector>
 
-struct file_info {
+class file_info {
+public:
   using list_type = std::vector<file_info*>;
+  using const_iterator = list_type::const_iterator;
+  using size_type = size_t;
+  using name_set_type = PJL::unordered_char_ptr_set;
+
+  file_info( char const *path_name, unsigned dir_index, size_t file_size,
+             char const *title, unsigned num_words = 0 );
+
+  file_info( unsigned char const *ptr_into_index_file );
+
+  unsigned dir_index() const {
+    return dir_index_;
+  }
+
+  char const* file_name() const {
+    return file_name_;
+  }
+
+  unsigned num_words() const {
+    return num_words_;
+  }
+
+  size_type size() const {
+    return file_size_;
+  }
+
+  char const* title() const {
+    return title_;
+  }
+
+  static const_iterator begin() {
+    return list_.begin();
+  }
+
+  static const_iterator end() {
+    return list_.end();
+  }
+
+  static unsigned current_index() {
+    return static_cast<unsigned>( list_.size() - 1 );
+  }
+
+  static void inc_words() {
+    ++list_.back()->num_words_;
+  }
 
   static file_info* ith_info( unsigned i ) {
     return list_[i];
   }
 
+  static size_type num_files() {
+    return list_.size();
+  }
+
+  static bool seen_file( char const *file_name ) {
+    return name_set_.find( file_name ) != name_set_.end();
+  }
+
+private:
+  unsigned const        dir_index_;
+  char const *const     file_name_;
+  size_type const       file_size_;
+  unsigned              num_words_;
+  char const *const     title_;
+
   static list_type      list_;
+  static name_set_type  name_set_;
 };
 
-#endif /* swishxx+file_info_hpp */
+#endif /* file_info_hpp */
 /* vim:set et sw=2 ts=2: */
--- test/data/hash.hpp
+++ test/data/hash.hpp
@@ -6,10 +6,15 @@
  * @sa https://github.com/paul-j-lucas/swishxx
  */
 
-#ifndef pjl_hash_H
-#define pjl_hash_H
+#ifndef pjl_hash_hpp
+#define pjl_hash_hpp
 
 #include <cstddef>
+#include <cstring>
+#include <functional>
+#include <unordered_set>
+
+namespace PJL {
 
 constexpr size_t Hash_Init = 14695981039346656037ul;
 
@@ -20,5 +25,34 @@ inline size_t hash_bytes( T const &value ) {
   return hash_bytes( &value, sizeof( value ) );
 }
 
-#endif /* pjl_hash_H */
+size_t hash_string( char const *s, size_t init = Hash_Init );
+
+} // namespace PJL
+
+namespace std {
+
+template<>
+inline size_t hash<char const*>::operator()( char const *s ) const noexcept {
+  return PJL::hash_string( s );
+}
+
+} // namespace std
+
+namespace PJL {
+
+struct char_ptr_equal_to {
+  using argument_type = char const*;
+  using result_type = bool;
+
+  result_type operator()( argument_type i, argument_type j ) const {
+    return std::strcmp( i, j ) == 0;
+  }
+};
+
+using unordered_char_ptr_set =
+  std::unordered_set<char const*,std::hash<char const*>,char_ptr_equal_to>;
+
+} // namespace PJL
+
+#endif  /* pjl_hash_hpp */
 /* vim:set et ts=2 sw=2: */
