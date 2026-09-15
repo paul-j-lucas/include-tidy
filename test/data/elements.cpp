7e51d0a71761c225d25465be9e87447e7fe84121
Paul J. Lucas <paul@lucasmail.org>
2026-09-14 19:15:59 -0700

    Improved C++ test.

--- test/data/elements.cpp
+++ test/data/elements.cpp
@@ -9,15 +9,22 @@
 #include "elements.hpp"
 
 element_map::element_map() {
-  static char const *const end_tag_table[] = {
-    "a",    "/a",
+  using cpcc = char const *const;
+
+  cpcc R = reinterpret_cast<char const*>( element::et_required  );
+
+  static char const *const END_TAG_TABLE[] = {
+    "a",  R,  "/a",
     // ...
     nullptr
   };
 
-  for ( auto p = end_tag_table; *p; ++p ) {
+  for ( auto p = END_TAG_TABLE; *p; ++p ) {
+    auto const v = (element::end_tag_type const)(long const)(p[1]);
+    auto &e = insert( value_type{ *p++, element{ v } } ).first->second;
+
     //   rv = std::pair<iterator,bool>
-    auto rv = insert( value_type{ *p++, 0 } );
+    auto rv = insert( value_type{ *p++, element{ v } } );
 
     //   it = std::map<K,V>::iterator
     auto it = rv.first;
--- test/data/elements.hpp
+++ test/data/elements.hpp
@@ -10,9 +10,20 @@
 #define element_map_hpp
 
 #include <map>
-#include <string>
 
-struct element_map : std::map<std::string,int> {
+struct element {
+  enum end_tag_type {
+    et_forbidden,
+    et_optional,
+    et_required
+  };
+
+  end_tag_type const end_tag;
+
+  explicit element( end_tag_type t ) : end_tag{ t } { }
+};
+
+struct element_map : std::map<char const*,element> {
   element_map();
 };
 
